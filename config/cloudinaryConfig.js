d6beb5d9a0879bb037de9333b400968ff1ec8285
YuriyVlasenko <yuriyvlasenko89@gmail.com>
2017-12-06 21:06:48 +0200

    add new cloudary accounts

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,3 +1,5 @@
+
+/*
 export default {
     cloudName: 'velobike',
     APIKey: '112284814369518',
@@ -6,3 +8,47 @@ export default {
     baseDeliveryUrl: 'http://res.cloudinary.com/velobike',
     APIBaseURL: 'https://api.cloudinary.com/v1_1/velobike/image/upload'    
 }
+*/
+//cloudinaryYV@gmail.com
+export default {
+    cloudName: 'dludxnhnb',
+    APIKey: '252127857363337',
+    APISecret: 'Szj8obhkZSlWMzhuXHGxxM3M2Vs',
+    environmentVariable: 'CLOUDINARY_URL=cloudinary://252127857363337:Szj8obhkZSlWMzhuXHGxxM3M2Vs@dludxnhnb',
+    baseDeliveryUrl: 'http://res.cloudinary.com/dludxnhnb',
+    APIBaseURL: 'https://api.cloudinary.com/v1_1/dludxnhnb/image/upload'    
+}
+
+//cloudinaryYV1@gmail.com
+/*
+export default {
+    cloudName: 'dryblwq7f',
+    APIKey: '946249371874378',
+    APISecret: 'IvhGKFyHlz-ujtkV4x6ZNgTwvhY',
+    environmentVariable: 'CLOUDINARY_URL=cloudinary://946249371874378:IvhGKFyHlz-ujtkV4x6ZNgTwvhY@dryblwq7f',
+    baseDeliveryUrl: 'http://res.cloudinary.com/dryblwq7f',
+    APIBaseURL: 'https://api.cloudinary.com/v1_1/dryblwq7f/image/upload'    
+}*/
+
+
+//cloudinaryYV2@gmail.com
+/*
+export default {
+    cloudName: 'dy9ohaciy',
+    APIKey: '424768927359216',
+    APISecret: 'MIwyTDVBYYudP5om-jgTxfJYfZI',
+    environmentVariable: 'CLOUDINARY_URL=cloudinary://424768927359216:MIwyTDVBYYudP5om-jgTxfJYfZI@dy9ohaciy',
+    baseDeliveryUrl: 'http://res.cloudinary.com/dy9ohaciy',
+    APIBaseURL: 'https://api.cloudinary.com/v1_1/dy9ohaciy/image/upload'    
+}*/
+
+//cloudinaryyv3@gmail.com
+/*
+export default {
+    cloudName: 'dk6bzcdpi',
+    APIKey: '836951167467934',
+    APISecret: 'nwaxrKsOLrOZEEXRFSv1VTQJHcE',
+    environmentVariable: 'CLOUDINARY_URL=cloudinary://836951167467934:nwaxrKsOLrOZEEXRFSv1VTQJHcE@dk6bzcdpi',
+    baseDeliveryUrl: 'http://res.cloudinary.com/dk6bzcdpi',
+    APIBaseURL: 'https://api.cloudinary.com/v1_1/dk6bzcdpi/image/upload'    
+}*/
--- src/app/Config/cloudinaryConfiguration.ts
+++ src/app/Config/cloudinaryConfiguration.ts
@@ -1,4 +1,35 @@
+/*
 export default {
     cloudName: 'velobike',
     uploadPreset: 'mwgx6hsr'
+}*/
+
+//cloudinaryYV@gmail.com
+export default {
+    cloudName: 'dludxnhnb',
+    uploadPreset: 'oeml6end'
 }
+
+//cloudinaryYV1@gmail.com
+/*
+export default {
+    cloudName: 'dryblwq7f',
+    uploadPreset: 'qjmvzmhd'
+}
+*/
+
+//cloudinaryYV2@gmail.com
+/*
+export default {
+    cloudName: 'dy9ohaciy',
+    uploadPreset: 'cghk2mha'
+}
+*/
+
+//cloudinaryYV3@gmail.com
+/*
+export default {
+    cloudName: 'dk6bzcdpi',
+    uploadPreset: 'tg7egip2'
+}
+*/
